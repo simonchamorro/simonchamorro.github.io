c35da526748b6dc6ee36617723e4f5bb5e57dd95
simonchamorro <simon.chamorro@usherbrooke.ca>
2020-04-26 23:47:30 -0400

    initial commit

---
+++ precache-manifest.3c7db7427d6b2a6837645ce9c8cf4182.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b764955c5957ab6903de82e0c3f05d30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e7060423c389d55746a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6192d13edc66c720d0f2",
+    "url": "/static/js/2.265fbdfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.265fbdfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e7060423c389d55746a",
+    "url": "/static/js/main.c39231da.chunk.js"
+  },
+  {
+    "revision": "7340797c3d775c8495e4",
+    "url": "/static/js/runtime-main.d7d89e4f.js"
+  }
+]);
